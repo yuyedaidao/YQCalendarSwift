b9735a3e19ff5d36ccf29cf4fe2b2290064a7c25
padding <wyqpadding@gmail.com>
2016-06-20 19:59:44 +0800

    add YQDayModel

---
+++ Classes/YQDayModel.swift
@@ -0,0 +1,16 @@
+//
+//  YQDayModel.swift
+//  YQCalendarSwift
+//
+//  Created by Wang on 16/6/20.
+//  Copyright © 2016年 王叶庆. All rights reserved.
+//
+
+import UIKit
+
+class YQDayModel: NSObject {
+    
+    init(indexPath: NSIndexPath, mode: YQCalendarMode) {
+        
+    }
+}
